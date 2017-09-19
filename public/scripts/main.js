d967577a499e167cdd3b685dc89182b4255c9a49
Amr Lotfy <amr.lotfy.othman@gmail.com>
2017-09-19 13:11:05 +0200

    Add verse count variable and data

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -13,6 +13,123 @@ var SuraNamesAr = [];
 var sortedTimestampSuraArray = [];
 var update_stamp = 0;
 
+var suraVerseCount = [
+    7,
+    286,
+    200,
+    176,
+    120,
+    165,
+    206,
+    75,
+    129,
+    109,
+    123,
+    111,
+    43,
+    52,
+    99,
+    128,
+    111,
+    110,
+    98,
+    135,
+    112,
+    78,
+    118,
+    64,
+    77,
+    227,
+    93,
+    88,
+    69,
+    60,
+    34,
+    30,
+    73,
+    54,
+    45,
+    83,
+    182,
+    88,
+    75,
+    85,
+    54,
+    53,
+    89,
+    59,
+    37,
+    35,
+    38,
+    29,
+    18,
+    45,
+    60,
+    49,
+    62,
+    55,
+    78,
+    96,
+    29,
+    22,
+    24,
+    13,
+    14,
+    11,
+    11,
+    18,
+    12,
+    12,
+    30,
+    52,
+    52,
+    44,
+    28,
+    28,
+    20,
+    56,
+    40,
+    31,
+    50,
+    40,
+    46,
+    42,
+    29,
+    19,
+    36,
+    25,
+    22,
+    17,
+    19,
+    26,
+    30,
+    20,
+    15,
+    21,
+    11,
+    8,
+    8,
+    19,
+    5,
+    8,
+    8,
+    11,
+    11,
+    8,
+    3,
+    9,
+    5,
+    4,
+    7,
+    3,
+    6,
+    3,
+    5,
+    4,
+    5,
+    6
+];
+
 /**
  * Normal Quran Order: 0
  * 
@@ -274,7 +391,7 @@ function addSuraCells() {
                 this.removeChild(this.childNodes.item(1));
             }
             this.childNodes.item(0).style.color = "rgb(0,0,0)";
-        }
+        };
 
         document.getElementById('reviews').appendChild(element);
     }
@@ -618,11 +735,11 @@ var suraCharCount = [
 
 function readableFormat(number) {
     if (number >= 1000000000) {
-        return (number / 1000000000).toFixed(2) + "G"
+        return (number / 1000000000).toFixed(2) + "G";
     } else if (number >= 1000000) {
-        return (number / 1000000).toFixed(2) + "M"
+        return (number / 1000000).toFixed(2) + "M";
     } else if (number >= 1000) {
-        return (number / 1000).toFixed(2) + "K"
+        return (number / 1000).toFixed(2) + "K";
     } else {
         return number;
     }
@@ -637,8 +754,8 @@ function todayStartTimeStamp() {
 }
 
 function getScore() {
-    var total = new Number(0);
-    var today = new Number(0);
+    var total = 0;
+    var today = 0;
     var todayStart = todayStartTimeStamp();
     for (i = 1; i <= 114; i++) {
         var suraScore = suraCharCount[i - 1];
