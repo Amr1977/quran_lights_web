9b19ae6542c12de9ef96e5cd7a6b60bcff40b4dc
Amr Lotfy <amr.lotfy@incorta.com>
2020-03-20 17:05:34 +0200

    Add kashing machine sound effect

--- public/scripts/audio.js
+++ public/scripts/audio.js
@@ -1,4 +1,6 @@
-var audio = new Audio("001002.mp3");
+const money_sound = "kaching.mp3";
+const hamd_sound = "001002.mp3";
+var audio = new Audio(money_sound);
 function playSuraRefreshSound() {
   if (audio.paused) {
     audio.play();
