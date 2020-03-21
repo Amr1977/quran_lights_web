0b9e8ab8f1bed621acd7e56b32d322755b4f1005
Amr Lotfy <amr.lotfy@incorta.com>
2020-03-21 07:49:36 +0200

    Add refresh sound on fetching updates

--- public/scripts/init_cells.js
+++ public/scripts/init_cells.js
@@ -108,6 +108,9 @@ function initCells() {
       bounceList.forEach(function (suraIndex) {
         bounce(suraIndex);
       });
+      if(bounceList.length) {
+        playSuraRefreshSound();
+      }
     });
   }
 }
