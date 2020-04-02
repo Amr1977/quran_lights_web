104ca1e283b99c067e0e63492c38882248b9d87c
Amr Lotfy <amr.lotfy@incorta.com>
2020-04-02 09:00:34 +0200

    concat all js files into a single js file after any mofification invoke minifi.sh, it will cancat alljs files and cache-bust in index.html

---
+++ public/minify.sh
@@ -0,0 +1,29 @@
+cd scripts
+rm quran_lights.min.js
+cat suras_data.js > quran_lights.min.js
+cat constants.js >> quran_lights.min.js
+cat utils.js >> quran_lights.min.js
+cat state.js >> quran_lights.min.js
+cat sync.js >> quran_lights.min.js
+cat main.js >> quran_lights.min.js
+cat init_app.js >> quran_lights.min.js
+cat init_cells.js >> quran_lights.min.js
+cat keyboard.js >> quran_lights.min.js
+cat memorization.js >> quran_lights.min.js
+cat bright_days_dark_days_chart.js >> quran_lights.min.js
+cat sort.js >> quran_lights.min.js
+cat treemap.js >> quran_lights.min.js
+cat guage.js >> quran_lights.min.js
+cat khatma.js >> quran_lights.min.js
+cat time_series.js >> quran_lights.min.js
+cat import_and_export.js >> quran_lights.min.js
+cat audio.js >> quran_lights.min.js
+cat account.js >> quran_lights.min.js
+cat selection.js >> quran_lights.min.js
+cat score.js >> quran_lights.min.js
+cat cells.js >> quran_lights.min.js
+cat login.js >> quran_lights.min.js
+cat charts.js >> quran_lights.min.js
+cat radar.js >> quran_lights.min.js
+cd ..
+./cache_buster.sh
