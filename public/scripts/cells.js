81fc4ea7c49ab4a501cf9c6ee3f4df3e1e331d58
Amr Lotfy <amr.lotfy@incorta.com>
2020-04-03 08:53:48 +0200

    Embed ayat iframe remove shift-click event

--- public/scripts/cells.js
+++ public/scripts/cells.js
@@ -182,7 +182,7 @@ function do_click() {
     $(".sura-" + event.index).addClass("animated bounceIn");
     toggle_memorization(event.index);
   } else if(event.shift_pressed) {
-    window.open("http://quran.ksu.edu.sa/index.php?l=en#aya=" + event.index + "_1&m=hafs&qaree=tunaiji&trans=en_sh");
+    // window.open("http://quran.ksu.edu.sa/index.php?l=en#aya=" + event.index + "_1&m=hafs&qaree=tunaiji&trans=en_sh");
   } else {
     toggle_select(event.index);
   }
