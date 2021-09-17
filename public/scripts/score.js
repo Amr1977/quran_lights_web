23e8e8a1833b05549ace5a3edccf71af7fd65ae5
Amr Lotfy <amr.lotfy@incorta.com>
2021-09-17 23:22:55 +0200

    Fix targeted daily read amount

--- public/scripts/score.js
+++ public/scripts/score.js
@@ -55,7 +55,7 @@ function getScore() {
     document.getElementById("score").textContent = "Total Balance: " + readableFormat(score_array[0]) + ", Verses: " + format_readable_number(scores["total_verses"]);
     document.getElementById("today_score").textContent = "Today Revenue: " + readableFormat(score_array[1]) + ", Verses: " + format_readable_number(scores["today_verses"]);
     document.getElementById("today_review_score").textContent = "* Review Revenue: " + readableFormat(review_today) + " of [" + readableFormat(memorization_state["memorized"] / get_memorized_refresh_period_days()) +"]";
-    document.getElementById("today_read_score").textContent = "* Read Revenue: " + readableFormat(score_array[3]) + " of [" + readableFormat(memorization_state["not_memorized"] / get_refresh_period_days()) +"]";;
+    document.getElementById("today_read_score").textContent = "* Read Revenue: " + readableFormat(score_array[3]) + " of [" + readableFormat((memorization_state["not_memorized"] + memorization_state["was_memorized"] + memorization_state["being_memorized"]) / get_refresh_period_days()) +"]";;
     document.getElementById("review_debt").textContent = readableFormat(debts["review"]);
     document.getElementById("read_debt").textContent = readableFormat(debts["read"]);
     document.getElementById("total_debt").textContent = readableFormat(debts["read"] + debts["review"]);
