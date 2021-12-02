1ee60f02d5c28fd830b432d7fc643636b2640d2a
Amr Lotfy <amr.lotfy@incorta.com>
2021-12-02 09:25:47 +0200

    WIP add landing and sign in pages

---
+++ public/dashboard/scripts/init_app.js
@@ -0,0 +1,68 @@
+/**
+ * initApp handles setting up UI event listeners and registering Firebase auth listeners:
+ *  - firebase.auth().onAuthStateChanged: This listener is called when the user is signed in or
+ *    out, and that is where we update the UI.
+ */
+function initApp() {
+  // Listening for auth state changes.
+  // [START authstatelistener]
+
+  if (!(document.utils && document.constants && document.suras_data && document.state)) {
+    console.log("Scheduled initApp after 1 second");
+    setTimeout(initApp, 1000);
+  }
+  console.log("initApp invoked...");
+
+  firebase.auth().onAuthStateChanged(function (user) {
+    if (user) {
+
+      Highcharts.setOptions({
+        chart: {
+            style: {
+                fontFamily: 'tahoma'
+            }
+        }
+    });
+
+      skew();
+      dispatch_uploads();
+      document.title = "Quran Lights [" + user.email + "]";
+      show_sign_in_only_elements();
+      //document.getElementById("sign-in-with-facebook").style.display = "none";
+      //document.getElementById("sign-in-with-twitter").style.display = "none";
+      var update_timestamp_ref = firebase.database()
+        .ref("users/" + firebase.auth().currentUser.uid + "/Master/update_stamp");
+      update_timestamp_ref.on("value", function (snapshot) {
+        var updatedValue = snapshot.val();
+        var indexOfTimeStamp = ownTimeStamps.indexOf(updatedValue);
+        if (indexOfTimeStamp != -1) {
+          //delete matching own timestamp
+          ownTimeStamps.splice(indexOfTimeStamp, 1);
+          return;
+        }
+        //stabilize successive triggers
+        if (timeStampTriggerTimerRef != null) {
+          clearTimeout(timeStampTriggerTimerRef);
+          console.log("Dropped repeated timestamp trigger.. ");
+        }
+        timeStampTriggerTimerRef = setTimeout(onTimeStampUpdated, isFirstLoad == 1 ? 0 : 5000);
+        isFirstLoad = 0;
+      });
+    }
+    else {
+      document.title = "Quran Lights";
+      clearInterval(periodicRefreshTimerRef);
+      hide_sign_in_only_elements();
+      //document.getElementById("sign-in-with-facebook").style.display = "block";
+      //document.getElementById("sign-in-with-twitter").style.display = "block";
+      // User is signed out.
+ 
+      //empty score
+      document.getElementById("score").textContent = "--";
+      //document.getElementById('quickstart-account-details').textContent = 'null';
+    }
+  });
+  // [END authstatelistener]
+  //document.getElementById('sign-in-with-facebook').addEventListener('click', signInWithFacebook, false);
+  //document.getElementById('sign-in-with-twitter').addEventListener('click', signInWithTwitter, false);
+}
---
+++ public/dashboard/scripts/main.js
@@ -0,0 +1,147 @@
+//TODO fix for new FDB structure
+//TODO put in upload queue and schedule a dispatcher function
+function refreshSura(suraIndex, refreshTimeStamp) {
+  //TODO update model
+  //TODO check for empty history array
+
+  //update FDB
+  var transaction_record = {
+    op: "refresh",
+    sura: suraIndex,
+    time: refreshTimeStamp
+  };
+
+  var transactions_records = [];
+  transactions_records.push(transaction_record);
+  add_to_transactions_history(transactions_records);
+
+  enqueue_for_upload(transaction_record);
+  surasHistory[suraIndex].history.push(transaction_record.time);
+  sortedTimestampSuraArray = [];
+  refreshCountSortedSuraArray = [];
+  playSuraRefreshSound();
+  add_sura_cells();
+  animate_score_elements();
+}
+
+function unrefresh(sura_index){
+  //TODO: complete it!
+  // console.log("sura_index ", sura_index);
+  // var history_size = surasHistory[sura_index].history.length;
+  // var refreshTimeStamp = surasHistory[sura_index].history(history_size - 1);
+  // var transaction_record = {
+  //   op: "unrefresh",
+  //   sura: sura_index,
+  //   time: refreshTimeStamp
+  // };
+  // //TODO: complete it!
+  console.log("TODO: unrefresh selected sura: remove refresh entry ", transaction_record);
+}
+
+//TODO animate ony after score change
+function animate_score_elements(){
+  $(".score").addClass("animated bounceIn");
+}
+
+function refreshByName(suraName) {
+  refreshSura(SuraNamesEn.indexOf(suraName) + 1, Math.floor(Date.now() / 1000))
+}
+
+var buildingSurasFlag = false;
+
+// firebase.initializeApp(config);
+
+var timeStampTriggerTimerRef = null;
+var periodicRefreshTimerRef = null;
+var isFirstLoad = 1;
+
+window.onload = function () {
+  // Sentry.init({ dsn: 'https://55c264ec9a484103890f2ca7ad8a4543@sentry.io/238887' });
+  init_collapsibles();
+  initApp();
+};
+
+function set_light_days() {
+  var light_days_selection_element = document.getElementById("light_days");
+  var light_days_count = Number(light_days_selection_element.value);
+  set_refresh_period_days(light_days_count);
+  add_sura_cells();
+}
+
+function set_memorized_light_days() {
+  var memorized_light_days_selection_element = document.getElementById("memorized_light_days");
+  var memorized_light_days_count = Number(memorized_light_days_selection_element.value);
+  set_memorized_refresh_period_days(memorized_light_days_count);
+  add_sura_cells();
+}
+
+function setup_light_days_options() {
+  var light_days_selection_element = document.getElementById("light_days");
+  light_days_selection_element.innerHTML = "";
+  for (var i = 1; i < 31; i++) {
+    var option = document.createElement("option");
+    option.value = i;
+    option.text = i;
+    light_days_selection_element.appendChild(option);
+  }
+
+  light_days_selection_element.value = get_refresh_period_days();
+}
+
+function setup_memorized_light_days_options(){
+  var memorized_light_days_selection_element = document.getElementById("memorized_light_days");
+  memorized_light_days_selection_element.innerHTML = "";
+  for (var i = 1; i < 31; i++) {
+    var option = document.createElement("option");
+    option.value = i;
+    option.text = i;
+    memorized_light_days_selection_element.appendChild(option);
+  }
+
+  memorized_light_days_selection_element.value = get_memorized_refresh_period_days();
+}
+
+function setup_reverse_sort_order_checkbox(){
+  document.getElementById('reverse_sura_sort_order_checkbox').checked = get_reverse_sort_order();
+}
+
+function show_sign_in_only_elements() {
+  for (var i = 0; i < SIGN_IN_ONLY_ELEMENTS.length; i++) {
+    var element = document.getElementById(SIGN_IN_ONLY_ELEMENTS[i]);
+    if(element) element.style.display = "block";
+  }
+
+  for (var i = 0; i < SIGN_OUT_ONLY_ELEMENTS.length; i++) {
+    var element = document.getElementById(SIGN_OUT_ONLY_ELEMENTS[i]);
+    if(element) element.style.display = "none";
+  }
+}
+
+function hide_sign_in_only_elements(){
+  for (var i = 0; i < SIGN_IN_ONLY_ELEMENTS.length; i++) {
+    var element = document.getElementById(SIGN_IN_ONLY_ELEMENTS[i]);
+    if(element) element.style.display = "none";
+  }
+
+  for (var i = 0; i < SIGN_OUT_ONLY_ELEMENTS.length; i++) {
+    var element = document.getElementById(SIGN_OUT_ONLY_ELEMENTS[i]);
+    if(element) element.style.display = "block";
+  }
+}
+
+function init_collapsibles() {
+  var coll = document.getElementsByClassName("collapsible");
+  var i;
+
+  for (i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function () {
+      this.classList.toggle("active");
+      var content = this.nextElementSibling;
+      if (content.style.display === "block") {
+        content.style.display = "none";
+      } else {
+        content.style.display = "block";
+      }
+    });
+  }
+}
