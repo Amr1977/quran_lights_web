fdbc475b5ddf8b160297d3041cf14b6f0de98f4f
Amr Lotfy <amr.lotfy.othman@gmail.com>
2021-12-07 01:06:22 +0200

    Add sign-in with google

--- public/login/js/login.js
+++ public/login/js/login.js
@@ -93,3 +93,101 @@ function sign_up(email, password) {
       console.log(error);
     });
 }
+
+
+function sign_in_with_google() {
+  signInWithOAuth("google");
+}
+
+function signInWithOAuth(signInProvider) {
+
+  add_auth_handler();
+  switch (signInProvider) {
+    case "google":
+      var provider = new firebase.auth.GoogleAuthProvider();
+      break;
+
+    case "facebook":
+      provider = new firebase.auth.FacebookAuthProvider();
+      break;
+
+    case "twitter":
+      provider = new firebase.auth.TwitterAuthProvider();
+      break;
+  }
+
+  firebase
+    .auth()
+    .signInWithPopup(provider)
+    .then(function(result) {
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      var token = result.credential.accessToken;
+      // The signed-in user info.
+      user = result.user;
+      // ...
+    })
+    .catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // The email of the user's account used.
+      var email = error.email;
+      // The firebase.auth.AuthCredential type that was used.
+      var credential = error.credential;
+      // ...
+    });
+}
+
+function sign_in_with_facebook() {
+  signInWithOAuth("facebook");
+}
+
+function signInWithTwitter() {
+  signInWithOAuth("twitter");
+}
+
+/**
+ * Sends an email verification to the user.
+ */
+function sendEmailVerification() {
+  // [START sendemailverification]
+  firebase
+    .auth()
+    .currentUser.sendEmailVerification()
+    .then(function() {
+      // Email Verification sent!
+      // [START_EXCLUDE]
+      alert("Email Verification Sent!");
+      // [END_EXCLUDE]
+    });
+  // [END sendemailverification]
+}
+function sendPasswordReset() {
+  var email = document.getElementById("email").value;
+  // [START sendpasswordemail]
+  firebase
+    .auth()
+    .sendPasswordResetEmail(email)
+    .then(function() {
+      // Password Reset Email Sent!
+      // [START_EXCLUDE]
+      alert("Password Reset Email Sent!");
+      // [END_EXCLUDE]
+    })
+    .catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // [START_EXCLUDE]
+      if (errorCode == "auth/invalid-email") {
+        alert(errorMessage);
+      } else if (errorCode == "auth/user-not-found") {
+        alert(errorMessage);
+      }
+      console.log(error);
+      // [END_EXCLUDE]
+    });
+  // [END sendpasswordemail];
+}
+
+document.account = true;
