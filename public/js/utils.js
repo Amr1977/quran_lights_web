1cd25bd3b38d91a143d53e7f2b46da7d21261b43
Amr Lotfy <amr.lotfy.othman@gmail.com>
2026-04-16 12:34:44 +0200

    fix: Add timeout to auto-dismiss toast messages

--- public/js/utils.js
+++ public/js/utils.js
@@ -14,6 +14,9 @@ function showToast(message) {
   var x = document.getElementById("snackbar");
   x.textContent = message;
   x.className = "show";
+  setTimeout(function() {
+    x.className = x.className.replace("show", "");
+  }, 3000);
 }
 
 function hideToast() {
